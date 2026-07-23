f38b14ce2bddd65d67b9123e2415fbe37b4053e2
Vidhi Manmode <vidhimanmode@Vidhis-MacBook-Air.local>
2026-07-23 23:31:08 +0530

    second commit

---
+++ variablesanddatatypes.py
@@ -0,0 +1,8 @@
+name = "vidhi" #string variable
+age = 19 #integer 
+height = 5.6 #float
+is_employed = False #boolean 
+print("my name is",name)
+print("my age is",age)
+print("my height is",height)
+print("am I employed?",is_employed)
